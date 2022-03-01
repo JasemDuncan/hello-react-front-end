ab8cbf3566bf7cec7d0910acea6c408ac0d94b0b
Jasem Duncan <jasemvalencia@gmail.com>
2022-02-28 20:04:09 -0500

    fix linterns

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <div>          
+        <div>
           <BrowserRouter>
             <Routes>
               <Route exact path="/" element={<Greeting />} />
